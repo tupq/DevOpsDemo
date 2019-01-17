7ce443cb41fbb9548a3b52ccbb60a73fb70e90aa
phan quang tu <tu.phan@vti.com.vn>
2019-01-17 21:58:59 +0700

    update test

--- DevOpsDemo/Controllers/SampleDataController.cs
+++ DevOpsDemo/Controllers/SampleDataController.cs
@@ -11,7 +11,7 @@ namespace DevOpsDemo.Controllers
     {
         private static string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "hahahaha"
         };
 
         [HttpGet("[action]")]
