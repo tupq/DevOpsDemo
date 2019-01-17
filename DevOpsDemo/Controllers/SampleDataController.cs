d7994092e7e12c478370ab4f0ecb87167259cd47
phan quang tu <tu.phan@vti.com.vn>
2019-01-17 22:40:55 +0700

    error again

--- DevOpsDemo/Controllers/SampleDataController.cs
+++ DevOpsDemo/Controllers/SampleDataController.cs
@@ -18,7 +18,7 @@ namespace DevOpsDemo.Controllers
         public IEnumerable<WeatherForecast> WeatherForecasts()
         {
             var rng = new Random();
-            x
+            xyxxxx
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 DateFormatted = DateTime.Now.AddDays(index).ToString("d"),
